120789bfbeb409b1f01b4e6ad9ced837d9fd3596
Matt <cunning.distraction@googlemail.com>
2024-05-21 00:33:02 +0100

    complete task 3

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -2,14 +2,22 @@ from milestone_2 import word
 
 print(word)
 
-while True:
-  guess = input("Guess a letter, and hit enter: ")
 
-  if len(guess) == 1 and guess.isalpha():
-    if guess in word:
-      print(f"Good guess! {guess} is in the word")
-    else:
-      print(f"Sorry, {guess} is not in the word. Try again")
-    break
+def check_guess(guess):
+  guess = guess.lower()
+  if guess in word:
+    print(f"Good guess! {guess} is in the word")
   else:
-    print("Invalid letter. Please enter a single, alphabetical character")
+    print(f"Sorry, {guess} is not in the word. Try again")
+
+
+def ask_for_input():
+  while True:
+    guess = input("Guess a letter, and hit enter: ")
+    if len(guess) == 1 and guess.isalpha():
+      break
+    else:
+      print("Invalid letter. Please enter a single, alphabetical character")
+  check_guess(guess)
+
+ask_for_input()
