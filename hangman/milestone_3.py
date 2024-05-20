e68a2bdc17f386e369dd4529d9aa1701632d5d34
Matt <cunning.distraction@googlemail.com>
2024-05-21 00:17:01 +0100

    complete task 2 to check if guess is present in word

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -6,7 +6,10 @@ while True:
   guess = input("Guess a letter, and hit enter: ")
 
   if len(guess) == 1 and guess.isalpha():
-    print("Good guess")
+    if guess in word:
+      print(f"Good guess! {guess} is in the word")
+    else:
+      print(f"Sorry, {guess} is not in the word. Try again")
     break
   else:
     print("Invalid letter. Please enter a single, alphabetical character")
