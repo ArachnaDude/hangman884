1c6f2f6f0a115f6b03511bde9a9ec716f902aa0c
Matt <cunning.distraction@googlemail.com>
2024-05-20 22:43:25 +0100

    complete milestone2task4 by verifying user input

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -7,3 +7,12 @@ word = random.choice(word_list)
 print(word)
 
 guess = input("Guess a letter, and hit enter: ")
+
+
+while True:
+  if len(guess) == 1 and guess.isalpha():
+    print("Good guess")
+    break
+  else:
+    print("Oops! That is not a valid input")
+    guess = input("Guess a letter, and hit enter: ")
