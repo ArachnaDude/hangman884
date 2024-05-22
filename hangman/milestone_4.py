666180f8593a83e9539e19197e7a023b3bc77fcc
Matt <cunning.distraction@googlemail.com>
2024-05-22 17:19:52 +0100

    add correct value for word_guessed and num_letters inside __init__ method

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -6,8 +6,8 @@ class Hangman():
   def __init__(self, word_list, num_lives=5) -> None:
     self.word_list = word_list
     self.word = random.choice(word_list)
-    self.word_guessed = [] #Todo - list of the letters in the word with _ for each letter not guessed
-    self.num_letters = None #Todo - number of unique letters in the word that have not been guessed yet 
+    self.word_guessed = ["_" for letter in self.word]
+    self.num_letters = len(set(self.word))
     self.num_lives = num_lives
     self.list_of_guesses = []
 
