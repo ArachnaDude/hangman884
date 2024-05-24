e429eef32a062bb0d12e830208c3aaae0cba9f3f
Matt <cunning.distraction@googlemail.com>
2024-05-24 19:39:54 +0100

    complete task 1 by defining play_game() and testing win and lose functionionality

--- hangman/milestone_5.py
+++ hangman/milestone_5.py
@@ -23,6 +23,7 @@ class Hangman():
     else:
       self.num_lives -= 1
       print(f"Sorry, {guess} is not in the word. \n You have {self.num_lives} lives left")
+    print(self.word_guessed)
 
   def ask_for_input(self):
     while True:
@@ -30,12 +31,31 @@ class Hangman():
       guess = input("Guess a letter, and hit enter: \n")
       if len(guess) != 1 or not guess.isalpha():
         print("Invalid letter. Please enter a single alphabetical character.")
+        break
       elif guess in self.list_of_guesses:
         print("You already tried that letter")
+        break
       else:
         self.check_guess(guess)
         self.list_of_guesses.append(guess)
+        break
   
 
-h = Hangman(word_list)
-h.ask_for_input()
+
+def play_game(word_list):
+  num_lives = 5
+  game = Hangman(word_list, num_lives)
+  while True:
+    if game.num_lives == 0:
+      print("You lost")
+      break
+    elif game.num_letters > 0:
+      game.ask_for_input()
+    else:
+      print("You win")
+      break
+    
+
+
+
+play_game(word_list)
